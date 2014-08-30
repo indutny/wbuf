5a9d1fa596b109c35d609632cf720b730b760217
Fedor Indutny <fedor@indutny.com>
2014-08-30 23:57:29 +0400

    initial

---
+++ index.js
@@ -0,0 +1,181 @@
+var Buffer = require('buffer').Buffer;
+
+function WBuf() {
+  this.buffers = [];
+  this.toReserve = 0;
+  this.size = 0;
+  this.avail = 0;
+
+  this.last = null;
+  this.offset = 0;
+
+  // Mostly a constant
+  this.reserveRate = 64;
+}
+module.exports = WBuf;
+
+WBuf.prototype.reserve = function reserve(n) {
+  this.toReserve += n;
+};
+
+WBuf.prototype._ensure = function _ensure(n) {
+  if (this.avail >= n)
+    return;
+
+  if (this.toReserve < n) {
+    this.toReserve = Math.max(Math.max(this.reserveRate, n - this.avail),
+                              this.toReserve);
+  }
+
+  if (this.avail === 0)
+    this._next();
+};
+
+WBuf.prototype._next = function _next() {
+  var buf = new Buffer(this.toReserve);
+  this.toReserve = 0;
+
+  this.buffers.push(buf);
+  this.avail = buf.length;
+  this.offset = 0;
+  this.last = buf;
+};
+
+WBuf.prototype._move = function _move(n) {
+  this.size += n;
+  this.avail -= n;
+  if (this.avail === 0)
+    this.last = null;
+};
+
+WBuf.prototype.skip = function skip(n) {
+  if (n === 0)
+    return;
+  this._ensure(n);
+
+  var left = n;
+  while (left > 0) {
+    var toSkip = Math.min(left, this.avail);
+    left -= toSkip;
+    this.size += toSkip;
+    if (toSkip === this.avail) {
+      if (left !== 0)
+        this._next();
+    } else {
+      this.offset += toSkip;
+      this.avail -= toSkip;
+    }
+  }
+};
+
+WBuf.prototype.copyFrom = function copyFrom(buf) {
+  if (buf.length === 0)
+    return;
+  this._ensure(buf.length);
+
+  var len = buf.length;
+  var off = 0;
+  while (off < len) {
+    var toCopy = Math.min(len - off, this.avail);
+    buf.copy(this.last, this.offset, off, off + toCopy);
+    off += toCopy;
+    this.size += toCopy;
+    if (toCopy === this.avail) {
+      if (off !== len)
+        this._next();
+    } else {
+      this.offset += toCopy;
+      this.avail -= toCopy;
+    }
+  }
+};
+
+WBuf.prototype.writeUInt8 = function writeUInt8(v) {
+  this._ensure(1);
+
+  this.last[this.offset++] = v;
+  this._move(1);
+};
+
+WBuf.prototype.writeUInt16BE = function writeUInt16BE(v) {
+  this._ensure(2);
+
+  // Fast case - everything fits into the last buffer
+  if (this.avail >= 2) {
+    this.last.writeUInt16BE(v, this.offset, true);
+    this.offset += 2;
+
+  // One byte here, one byte there
+  } else {
+    this.last[this.offset] = (v >>> 8);
+    this._next();
+    this.last[this.offset++] = v & 0xff;
+  }
+
+  this._move(2);
+};
+
+WBuf.prototype.writeUInt24BE = function writeUInt24BE(v) {
+  this._ensure(3);
+
+  // Fast case - everything fits into the last buffer
+  if (this.avail >= 3) {
+    this.last.writeUInt16BE(v >>> 8, this.offset, true);
+    this.last[this.offset + 2] = v & 0xff;
+    this.offset += 3;
+    this._move(3);
+
+  // Two bytes here
+  } else if (this.avail >= 2) {
+    this.last.writeUInt16BE(v >>> 8, this.offset, true);
+    this._next();
+    this.last[this.offset++] = v & 0xff;
+    this._move(3);
+
+  // Just one byte here
+  } else {
+    this.last[this.offset] = v >>> 16;
+    this._move(1);
+    this._next();
+    this.writeUInt16BE(v & 0xffff);
+  }
+};
+
+WBuf.prototype.writeUInt32BE = function writeUInt32BE(v) {
+  this._ensure(4);
+
+  // Fast case - everything fits into the last buffer
+  if (this.offset + 4 <= this.last.length) {
+    this.last.writeUInt32BE(v, this.offset, true);
+    this.offset += 4;
+    this._move(4);
+
+  // Three bytes here
+  } else if (this.offset + 3 <= this.last.length) {
+    this.writeUInt24BE(v >>> 8);
+    this.last[this.offset++] = v & 0xff;
+    this._move(1);
+
+  // Slow case, who cares
+  } else {
+    this.writeUInt16BE(v >>> 16);
+    this.writeUInt16BE(v & 0xffff);
+  }
+};
+
+WBuf.prototype.render = function render() {
+  var left = this.size;
+  var out = [];
+
+  for (var i = 0; i < this.buffers.length && left >= 0; i++) {
+    var buf = this.buffers[i];
+    left -= buf.length;
+    if (left >= 0) {
+      out.push(buf);
+    } else {
+      out.push(buf.slice(0, left));
+    }
+  }
+
+  return out;
+};
---
+++ test/wbuf-test.js
@@ -0,0 +1,94 @@
+var assert = require('assert');
+var WriteBuffer = require('../');
+
+describe('WriteBuffer', function() {
+  var w;
+  beforeEach(function() {
+    w = new WriteBuffer();
+  });
+
+  function join(arr) {
+    return arr.map(function(buf) {
+      return buf.toString('hex');
+    }).join('');
+  }
+
+  describe('.writeUInt8', function() {
+    it('should write bytes', function() {
+      w.writeUInt8(1);
+      w.writeUInt8(2);
+      w.writeUInt8(3);
+      w.writeUInt8(4);
+      assert.equal(join(w.render()), '01020304');
+    });
+
+    it('should correctly handle overflow', function() {
+      w.reserve(3);
+      w.writeUInt8(1);
+      w.writeUInt8(2);
+      w.writeUInt8(3);
+      w.writeUInt8(4);
+      assert.equal(join(w.render()), '01020304');
+    });
+  });
+
+  describe('.writeUInt16BE', function() {
+    it('should write bytes', function() {
+      w.writeUInt16BE(0x0102);
+      w.writeUInt16BE(0x0304);
+      assert.equal(join(w.render()), '01020304');
+    });
+
+    it('should correctly handle overflow', function() {
+      w.reserve(2);
+      w.reserve(3);
+      w.writeUInt16BE(0x0102);
+      w.writeUInt16BE(0x0304);
+      w.writeUInt16BE(0x0506);
+      assert.equal(join(w.render()), '010203040506');
+    });
+  });
+
+  describe('.writeUInt24BE', function() {
+    it('should write bytes', function() {
+      w.writeUInt24BE(0x010203);
+      w.writeUInt24BE(0x040506);
+      assert.equal(join(w.render()), '010203040506');
+    });
+  });
+
+  describe('.writeUInt32BE', function() {
+    it('should write bytes', function() {
+      w.writeUInt32BE(0x01020304);
+      w.writeUInt32BE(0x05060708);
+      assert.equal(join(w.render()), '0102030405060708');
+    });
+  });
+
+  describe('.skip', function() {
+    it('should skip bytes', function() {
+      w.skip(4);
+      w.writeUInt32BE(0xdeadbeef);
+      assert(/^.{8}deadbeef$/.test(join(w.render())));
+    });
+  });
+
+  describe('.copyFrom', function() {
+    it('should copy bytes', function() {
+      var tmp = new Buffer(128);
+      for (var i = 0; i < tmp.length; i++)
+        tmp[i] = i;
+      w.writeUInt32BE(0xdeadbeef);
+      w.copyFrom(tmp);
+      w.writeUInt32BE(0xabbadead);
+
+      assert.equal(
+        join(w.render()),
+        'deadbeef000102030405060708090a0b0c0d0e0f101112131415161718191a1b' +
+        '1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b' +
+        '3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b' +
+        '5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b' +
+        '7c7d7e7fabbadead');
+    });
+  });
+});
